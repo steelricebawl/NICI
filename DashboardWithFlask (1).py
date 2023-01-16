95216be0f085a210251ef758595ab8b5a6fce530
steelricebawl <90228003+steelricebawl@users.noreply.github.com>
2023-01-16 16:10:17 +0900

    Update DashboardWithFlask (1).py

--- DashboardWithFlask (1).py
+++ DashboardWithFlask (1).py
@@ -14,7 +14,7 @@ import numpy as np
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
-
+server = app.server
 
 # def layout 
 app.layout = html.Div([
